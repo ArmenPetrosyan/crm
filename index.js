2ba514c425890af3385924f8a387f8700146f09e
ArmenPetrosyan <thebraincrasher@gmail.com>
2018-11-17 18:41:27 +0200

    Practice: project structure

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ app.get('/', (req, res) => {
   res.send(`Node server is running on port ${PORT}`);
 })
 
-
-app.listen(PORT, () => `Node server UP and running on port ${PORT}`);
+app.listen(PORT, () => console.log(`Node server UP and running on port ${PORT}`));
